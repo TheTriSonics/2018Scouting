aeb84504e6e7e60b2f7bcdfbd4aba76b021e2853
Stale-Bread <35582171+Stale-Bread@users.noreply.github.com>
2018-02-26 20:28:54 -0500

    02/17/18

--- app/src/main/java/com/example/morrie/frc2018scout/MainActivity.java
+++ app/src/main/java/com/example/morrie/frc2018scout/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,165 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner teamMembersSpinner = (Spinner) findViewById(R.id.scouterNameSpinner);
+        ArrayAdapter<CharSequence> TMadapt = ArrayAdapter.createFromResource(this, R.array.TeamMembers, android.R.layout.simple_spinner_item);
+        TMadapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamMembersSpinner.setAdapter(TMadapt);
+
+        Spinner teamNumbersSpinner = (Spinner) findViewById(R.id.teamNumberSpinner);
+        ArrayAdapter<CharSequence> TNadapt = ArrayAdapter.createFromResource(this, R.array.KetteringTeamNumbers, android.R.layout.simple_spinner_item);
+        TNadapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamNumbersSpinner.setAdapter(TNadapt);
+
+        Spinner eventSpinner = (Spinner) findViewById(R.id.eventSpinner);
+        ArrayAdapter<CharSequence> Eadapt = ArrayAdapter.createFromResource(this, R.array.Event, android.R.layout.simple_spinner_item);
+        Eadapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventSpinner.setAdapter(Eadapt);
+
+        Spinner startingLocationSpinner = (Spinner) findViewById(R.id.startingPositionSpinner);
+        ArrayAdapter<CharSequence> SLadapt = ArrayAdapter.createFromResource(this, R.array.StartingLocation, android.R.layout.simple_spinner_item);
+        SLadapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        startingLocationSpinner.setAdapter(SLadapt);
+
+        Spinner climbSpinner = (Spinner) findViewById(R.id.climbSpinner);
+        ArrayAdapter<CharSequence> Cadapt = ArrayAdapter.createFromResource(this, R.array.Climb, android.R.layout.simple_spinner_item);
+        Cadapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        climbSpinner.setAdapter(Cadapt);
+
+        EditText matchNum = (EditText) findViewById(R.id.matchNumberText);
+        Switch allianceColor = (Switch) findViewById(R.id.allianceColorSwitch);
+        CheckBox autoBaseline = (CheckBox) findViewById(R.id.baselineCheckBox);
+        EditText autoSwitchNum = (EditText) findViewById(R.id.autonSwitch);
+        EditText autoScaleNum = (EditText) findViewById(R.id.autonScale);
+        CheckBox autoIncorrectSwitch = (CheckBox) findViewById(R.id.incorrectColorCheckbox);
+        CheckBox autoBaseLine = (CheckBox) findViewById(R.id.baselineCheckBox);
+        EditText teleopOpponentSwitchNum = (EditText) findViewById(R.id.teleopOpponentSwitch);
+        EditText teleopScaleNum = (EditText) findViewById(R.id.teleopScale);
+        EditText teleopAllianceSwitchNum = (EditText) findViewById(R.id.teleopAllianceSwitch);
+        EditText vaultNum = (EditText) findViewById(R.id.vault);
+
+        Button autoSwitchP = (Button) findViewById(R.id.autonSwitchPlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(autoSwitchNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                autoSwitchNum.setText(result);
+            }
+        });
+
+        Button autoSwitchM = (Button) findViewById(R.id.autonSwitchMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(autoSwitchNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                autoSwitchNum.setText(result);
+            }
+        });
+
+        Button autoScaleP = (Button) findViewById(R.id.autonScalePlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(autoScaleNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                autoScaleNum.setText(result);
+            }
+        });
+
+        Button autoScaleM = (Button) findViewById(R.id.autonScaleMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(autoScaleNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                autoScaleNum.setText(result);
+            }
+        });
+
+        Button teleopOpponentSwitchP = (Button) findViewById(R.id.teleopOpponentSwitchPlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopOpponentSwitchNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                teleopOpponentSwitchNum.setText(result);
+            }
+        });
+
+        Button teleopOpponentSwitchM = (Button) findViewById(R.id.teleopOpponentSwitchMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopOpponentSwitchNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                teleopOpponentSwitchNum.setText(result);
+            }
+        });
+
+        Button teleopScaleP = (Button) findViewById(R.id.teleopScalePlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopScaleNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                teleopScaleNum.setText(result);
+            }
+        });
+
+        Button teleopScaleM = (Button) findViewById(R.id.teleopScaleMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopScaleNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                teleopScaleNum.setText(result);
+            }
+        });
+
+        Button teleopAllianceSwitchP = (Button) findViewById(R.id.teleopAllianceSwitchPlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopAllianceSwitchNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                teleopAllianceSwitchNum.setText(result);
+            }
+        });
+
+        Button teleopAllianceSwitchM = (Button) findViewById(R.id.teleopAllianceSwitchMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(teleopAllianceSwitchNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                teleopAllianceSwitchNum.setText(result);
+            }
+        });
+
+        Button vaultP = (Button) findViewById(R.id.vaultPlus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(vaultNum.getText().toString());
+            if (start < 10) {
+                start++;
+                String result = String.valueOf(start);
+                vaultNum.setText(result);
+            }
+        });
+
+        Button vaultM = (Button) findViewById(R.id.vaultMinus);
+        autoSwitchP.setOnClickListener(v -> {
+            int start = Integer.parseInt(vaultNum.getText().toString());
+            if (start > 0) {
+                start++;
+                String result = String.valueOf(start);
+                vaultNum.setText(result);
+            }
+        });
+
+
     }
 
     @Override
